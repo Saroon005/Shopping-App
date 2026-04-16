a6059db4ed45f77b6f85db6565f60b89795c076d
Saroon005 <aps05saroon@gmail.com>
2026-04-16 11:57:31 +0530

    Coded Product entity, repo and dto

--- src/main/java/com/group/shoppingapp/dto/ProductRequestDTO.java
+++ src/main/java/com/group/shoppingapp/dto/ProductRequestDTO.java
@@ -1,5 +1,50 @@
 package com.group.shoppingapp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class ProductRequestDTO {
 
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    private Double price;
+
+    private String description;
+
+    @NotBlank(message = "SKU is required")
+    private String sku;
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
 }
